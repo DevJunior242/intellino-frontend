9baa3f648fbc177d0677e089c4cba5f7b8aed728
DevJunior242 <devjunior677@gmail.com>
2026-04-13 17:50:51 +0000

    sa

--- src/Saas/pages/League/xx.jsx
+++
@@ -1,7 +0,0 @@
- CMD php artisan config:clear && \
-#     php artisan cache:clear && \
-#     php artisan config:cache && \
-#     php artisan route:cache && \
-#     php artisan view:cache && \
-#     php artisan migrate:fresh --seed --force && \
-#     php artisan serve --host=0.0.0.0 --port=8080
