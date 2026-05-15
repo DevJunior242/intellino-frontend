8d29871a74da5068d182bb2dbeff39d52378899f
DevJunior242 <devjunior677@gmail.com>
2026-05-15 09:30:26 +0000

    save

--- src/Saas/pages/League/competion/ConfigNotationCardDetails.jsx
+++ src/Saas/pages/League/competion/ConfigNotationCardDetails.jsx
@@ -178,7 +178,7 @@ export default function ConfigNotationCardDetails() {
         `/api/seances/configs/${configActive.id}/connecter-tablette`,
         { code_acces: pin },
       );
-      console.log("res", res);
+      console.log("connexion tablette", res);
       if (res.data.success) {
         localStorage.setItem("poste", res.data.poste);
         //localStorage.setItem("config_active_id", configActive.id);
