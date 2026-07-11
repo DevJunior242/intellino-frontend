4a0cdaddba6cea6f88ba5ec9e722522f21e3ab9b
DevJunior242 <devjunior677@gmail.com>
2026-07-11 11:04:45 +0000

    mod

--- src/Api/Axios.jsx
+++ src/Api/Axios.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-//const BASE_URL = import.meta.env.VITE_API_URL || "http://178.105.189.230";
+const BASE_URL = import.meta.env.VITE_API_URL || "http://178.105.189.230";
 
-const BASE_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
+//const BASE_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
 export const Instance = axios.create({
   baseURL: BASE_URL,
   timeout: 60000,
